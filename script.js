4f1d6a10813de14a6fe10147fccd414fdc0b87c3
Jubair Ibn Khaled <160339037+Pro-jubair@users.noreply.github.com>
2024-02-18 22:45:10 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+var num1 = prompt("Enter the value");
+var num2 = prompt("Enter the value");
+
+num1 = parseInt(num1,10);
+num2 = parseInt(num2,10);
+
+var sum,sub;
+
+sum = num1 + num2;
+document.write("jogfol = " + sum + "<br/>" );
+
+sub = num1 - num2;
+document.write("biyogfol = " + sub);
+
